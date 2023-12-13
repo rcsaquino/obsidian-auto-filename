595e41c2064e4e4bf2046022421e4a93955d7efa
Rey Christian <rcsaquino.md@gmail.com>
2023-12-13 18:46:25 +0800

    v1.0.0
    VERSION 1 RELEASE
    
    Changelog:
    - Rearranged settings
    - Improved target folder detection accuracy
    - Now supports all headers from H1 to H6
    - Code variable name changes
    - Added an option to only use first line of text when determining filename
    - Improved trimming of white space

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,30 @@
 import { Plugin, PluginSettingTab, Setting, Notice, TFile } from "obsidian";
 
 interface PluginSettings {
-	charCount: number;
 	targetFolder: string;
-	isTitleHidden: boolean;
-	checkInterval: number;
-	supportYAML: boolean;
-	useHeader: boolean;
 	includeSubfolder: boolean;
+	useHeader: boolean;
+	useFirstLine: boolean;
+	isTitleHidden: boolean;
+	supportYAML: boolean;
+	charCount: number;
+	checkInterval: number;
 }
 
 const DEFAULT_SETTINGS: PluginSettings = {
-	charCount: 50,
 	targetFolder: "",
-	isTitleHidden: false,
-	checkInterval: 500,
-	supportYAML: true,
-	useHeader: true,
 	includeSubfolder: false,
+	useHeader: true,
+	useFirstLine: false,
+	isTitleHidden: true,
+	supportYAML: true,
+	charCount: 50,
+	checkInterval: 500,
 };
 
-// Global variable for "Rename all files" setting
+// Global variables for "Rename all files" setting
 let renamedFileCount: number = 0;
+let tempNewPaths: string[] = [];
 
 // Variables for debounce
 let onTimeout: boolean = true;
@@ -31,7 +34,8 @@ let previousFile: string;
 function inTargetFolder(file: TFile, settings: PluginSettings): boolean {
 	if (settings.targetFolder == "") return false; // False if user has no target folder selected
 	if (settings.includeSubfolder) {
-		if (!file.parent?.path.startsWith(settings.targetFolder)) return false; // False if file is not in user's target folder or its subfolders
+		if (file.parent?.path.split("/")[0] != settings.targetFolder)
+			return false; // False if file is not in user's target folder or its subfolders
 	} else {
 		if (file.parent?.path != settings.targetFolder) return false; // False if file is not in user's target folder
 	}
@@ -45,7 +49,7 @@ export default class AutoFilename extends Plugin {
 	async renameFile(file: TFile, noDelay = false): Promise<void> {
 		if (!inTargetFolder(file, this.settings)) return; // Return if file is not within the target folder/s
 
-		// Debounce to avoid performance issues
+		// Debounce to avoid performance issues if noDelay is disabled
 		if (!noDelay) {
 			if (onTimeout) {
 				// Clear timeout only if renameFile is called on the same file.
@@ -75,41 +79,80 @@ export default class AutoFilename extends Plugin {
 		}
 
 		// Use the header as filename depending on user preference
-		if (this.settings.useHeader && content.startsWith("# ")) {
-			let index = content.indexOf("\n");
-			if (index != -1) content = content.slice(2, index);
+		if (this.settings.useHeader && content[0] == "#") {
+			const headerArr: string[] = [
+				"# ",
+				"## ",
+				"### ",
+				"##### ",
+				"###### ",
+			];
+			for (let i = 0; i < headerArr.length; i++) {
+				if (content.startsWith(headerArr[i])) {
+					let index = content.indexOf("\n");
+					if (index != -1) content = content.slice(i + 2, index);
+					break;
+				}
+			}
 		}
 
 		const allowedChars: string =
 			"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !#$%&'()+,-.;=@[]^_`{}~"; // Characters that are safe to use in a filename
-		let fileName: string = "";
+		let newFileName: string = "";
 
 		// Takes the first n characters of the file and uses it as part of the filename.
 		for (let i: number = 0; i < content.length; i++) {
 			// Adds "..." after the last character if file characters > n
 			if (i >= Number(this.settings.charCount)) {
-				fileName += "...";
+				newFileName = newFileName.trimEnd();
+				newFileName += "...";
 				break;
 			}
 			let char = content[i];
-			if (char === "\n") char = " "; // Treat new lines as spaces.
-			if (allowedChars.includes(char)) fileName += char;
+
+			if (char === "\n") {
+				// Ignore succeeding lines of text when determining filename depending on user preference.
+				if (this.settings.useFirstLine) {
+					newFileName = newFileName.trimEnd();
+					newFileName += "..."; // Adds "..." at the end to indicate there might be more text.
+					break;
+				}
+
+				char = " "; // Treat new lines as spaces.
+			}
+
+			if (allowedChars.includes(char)) newFileName += char;
 		}
 
-		fileName = fileName.trim(); // Trim white space
-		if (fileName[0] == ".") fileName = fileName.slice(1); // Remove if "." is the first character in a file to avoid naming issues.
+		newFileName = newFileName.trim(); // Trim white space
 
-		// No need to rename if new filename == old filename
-		// " (abc1234).md" = 13 chars
-		if (file.name.startsWith(fileName) && fileName != "") return;
+		// Remove all leading "." to avoid naming issues.
+		while (newFileName[0] == ".") {
+			newFileName = newFileName.slice(1);
+		}
+		if (newFileName == "") newFileName = "Untitled"; // Change to Untitled if newFileName outputs to nothing.
 
-		// Adds 7 random alphanumeric characters at the end.
-		// This allows multiple files with the same first n characters to be created without issues.
-		fileName = `${fileName} (${Math.random().toString(36).slice(-7)}).md`;
+		let newPath: string = `${file.parent?.path}/${newFileName}.md`;
 
-		const newPath: string = `${file.parent?.path}/${fileName}`;
+		// Duplicate checker: If file exists or newPath is in tempNewPaths, enter loop.
+		let counter: number = 1;
+		let fileExists: boolean =
+			this.app.vault.getAbstractFileByPath(newPath) != null;
+		while (fileExists || tempNewPaths.includes(newPath)) {
+			if (file.path == newPath) return; // No need to rename if new filename == old filename
+			counter += 1;
+			newPath = `${file.parent?.path}/${newFileName} (${counter}).md`; // Adds (2), (3), (...) to avoid filename duplicates similar to windows.
+			fileExists = this.app.vault.getAbstractFileByPath(newPath) != null;
+		}
+
+		// Populate tempNewPaths if noDelay is enabled to avoid duplicate bugs
+		if (noDelay) {
+			tempNewPaths.push(newPath);
+		}
+
+		// Rename file and increment renamedFileCount
 		await this.app.fileManager.renameFile(file, newPath);
-		renamedFileCount++;
+		renamedFileCount += 1;
 	}
 
 	async saveSettings(): Promise<void> {
@@ -203,31 +246,40 @@ class AutoFilenameSettings extends PluginSettingTab {
 
 		// Setting 3
 		new Setting(this.containerEl)
-			.setName("Character count")
+			.setName("Use the header as filename")
 			.setDesc(
-				"Auto Filename will use the first x number of characters in file as filename."
+				"Use the header as filename if the file starts with a header"
 			)
-			.addText((text) =>
-				text
-					.setPlaceholder(
-						`10-100 (Default: ${DEFAULT_SETTINGS.charCount})`
-					)
-					.setValue(String(this.plugin.settings.charCount))
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.useHeader)
 					.onChange(async (value) => {
-						const numVal = Number(value);
-						if (numVal >= 10 && numVal <= 100) {
-							this.plugin.settings.charCount = numVal;
-							await this.plugin.saveSettings();
-						}
-					})
-			);
+						this.plugin.settings.useHeader = value;
+						await this.plugin.saveSettings();
+					});
+			});
 
 		// Setting 4
+		new Setting(this.containerEl)
+			.setName("Only use the first line")
+			.setDesc(
+				"Ignore succeeding lines of text when determining filename."
+			)
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.useFirstLine)
+					.onChange(async (value) => {
+						this.plugin.settings.useFirstLine = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		// Setting 5
 		const shouldDisable: boolean =
 			!document.body.classList.contains("show-inline-title");
 		const description: string = shouldDisable
 			? 'Enable "Appearance > Advanced > Show inline title" in options to use this setting.'
-			: 'Overrides "Appearance > Advanced > Show inline title" for files on the target folder.';
+			: 'Override "Appearance > Advanced > Show inline title" for files on the target folder.';
 		new Setting(this.containerEl)
 			.setName("Hide inline title for target folder")
 			.setDesc(description)
@@ -261,7 +313,41 @@ class AutoFilenameSettings extends PluginSettingTab {
 				}
 			});
 
-		// Setting 5
+		// Setting 6
+		new Setting(this.containerEl)
+			.setName("YAML support")
+			.setDesc("Enables YAML support.")
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.supportYAML)
+					.onChange(async (value) => {
+						this.plugin.settings.supportYAML = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		// Setting 7
+		new Setting(this.containerEl)
+			.setName("Character count")
+			.setDesc(
+				"Auto Filename will use the first x number of characters in file as filename."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder(
+						`10-100 (Default: ${DEFAULT_SETTINGS.charCount})`
+					)
+					.setValue(String(this.plugin.settings.charCount))
+					.onChange(async (value) => {
+						const numVal = Number(value);
+						if (numVal >= 10 && numVal <= 100) {
+							this.plugin.settings.charCount = numVal;
+							await this.plugin.saveSettings();
+						}
+					})
+			);
+
+		// Setting 8
 		new Setting(this.containerEl)
 			.setName("Check interval")
 			.setDesc(
@@ -281,39 +367,11 @@ class AutoFilenameSettings extends PluginSettingTab {
 					})
 			);
 
-		// Setting 6
-		new Setting(this.containerEl)
-			.setName("Use the header as filename")
-			.setDesc(
-				"Uses the header as filename if the file starts with an H1."
-			)
-			.addToggle((toggle) => {
-				toggle
-					.setValue(this.plugin.settings.useHeader)
-					.onChange(async (value) => {
-						this.plugin.settings.useHeader = value;
-						await this.plugin.saveSettings();
-					});
-			});
-
-		// Setting 7
-		new Setting(this.containerEl)
-			.setName("YAML support")
-			.setDesc("Enables YAML support.")
-			.addToggle((toggle) => {
-				toggle
-					.setValue(this.plugin.settings.supportYAML)
-					.onChange(async (value) => {
-						this.plugin.settings.supportYAML = value;
-						await this.plugin.saveSettings();
-					});
-			});
-
-		// Setting 8
+		// Setting 9
 		new Setting(this.containerEl)
 			.setName("Rename all files")
 			.setDesc(
-				"Forcibly renames all files on the target folder. Warning: To be safe, make sure you backup before proceeding."
+				"Force rename all files on the target folder. Warning: To be safe, make sure you backup before proceeding."
 			)
 			.addButton((button) =>
 				button.setButtonText("Rename").onClick(async () => {
@@ -329,6 +387,7 @@ class AutoFilenameSettings extends PluginSettingTab {
 					);
 
 					renamedFileCount = 0;
+					tempNewPaths = [];
 					await Promise.all(
 						filesToRename.map((file: TFile) =>
 							this.plugin.renameFile(file, true)
